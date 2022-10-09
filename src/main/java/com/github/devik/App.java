7ead764bd7183a10b24953d1b5b68c12f8df5b8c
Vikas Choudhary <vikaschd@amazon.com>
2022-10-09 17:04:06 +0530

    initial commit

--- src/main/java/com/github/devik/App.java
+++ src/main/java/com/github/devik/App.java
@@ -1,13 +1,17 @@
-package org.example;
+package com.github.devik;
+
+import com.amazonaws.services.lambda.runtime.Context;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+    }
+
+    public void handle(String input, Context context) {
+        context.getLogger().log("Input: " + input);
     }
 }
